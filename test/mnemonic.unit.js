ea000ce2b93ab4ae6f8648ded8888d339f8ee8c7
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-11-18 22:30:30 -0500

    Update mnemonic.unit.js

--- test/mnemonic.unit.js
+++ test/mnemonic.unit.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var should = chai.should();
 
 var Mnemonic = require('..');
-var errors = require('@npccoin/npccoincore-lib').errors;
+var errors = require('bitcore-lib').errors;
 var bip39_vectors = require('./data/fixtures.json');
 
 describe('Mnemonic', function() {
